6fe978c28c313e15b380681b873e719f041f7154
Elina <elina.titomir@gmail.com>
2025-05-13 16:07:34 +0300

    and

--- js/task-1.js
+++ js/task-1.js
@@ -1,11 +1,10 @@
-'use strict';
-
-
 function makeTransaction(quantity, pricePerDroid) {
-    const quantity = 5;
-    const pricePerDroid = 3000;
-    const totalPrice = quantity * pricePerDroid; 
-    const greeting = `You ordered ${quantity} droids worth ${totalPrice} credits!`;
+    
+    const totalPrice = quantity * pricePerDroid;
+    return `You ordered ${quantity} droids worth ${totalPrice} credits!`
+      
 }
-console.log(makeTransaction(5, 3000));
 
+console.log(makeTransaction(5, 3000));
+console.log(makeTransaction(3, 1000));
+console.log(makeTransaction(10, 500));
